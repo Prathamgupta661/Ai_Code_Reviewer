71d49ea85d7b1212c4fbf1dad683364223d95189
Pratham Gupta <97107259+Prathamgupta661@users.noreply.github.com>
2025-05-03 19:01:28 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,7 +7,6 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      console.log('GOOGLE CLIENT ID:', import.meta.env.VITE_GOOGLE_CLIENT_ID);
     <App />
     </GoogleOAuthProvider>
   </StrictMode>,
